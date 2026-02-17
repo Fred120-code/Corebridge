438e7a395312c81c0ffff8315f62988076b3d0fe
Fred120-code <fredayemtsa@gmail.com>
2026-02-17 15:45:54 +0100

    first commit

---
+++ core_brige/app/layout.tsx
@@ -0,0 +1,52 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Kanit, Merriweather, Syne } from "next/font/google";
+import "./globals.css";
+
+const kanit = Kanit({
+  weight: ["200", "300", "400", "500", "600", "700"],
+  variable: "--font-kanit",
+  subsets: ["latin"],
+});
+
+const merriweather = Merriweather({
+  weight: ["400"],
+  variable: "--font-merriweather",
+  subsets: ["latin"],
+});
+
+const syne = Syne({
+  weight: ["400"],
+  variable: "--font-syne",
+  subsets: ["latin"],
+});
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Corebridge",
+  description: "Corebridge Business Plateform",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${kanit.variable} ${merriweather.variable} ${syne.variable}`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
