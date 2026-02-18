7ed51fa9ac65949cb61ea3455aa9b69542d94f9b
Fred120-code <fredayemtsa@gmail.com>
2026-02-18 18:51:59 +0100

    end of about section

--- core_brige/app/Pages/Index/Index.tsx
+++ core_brige/app/Pages/Index/Index.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Hero from './Hero/page'
 import Brands from './Brands/page'
+import About from './About/page';
 
 const Index = () => {
   return (
     <>
-        <Hero/>
-        <Brands/>
+      <Hero />
+      <Brands />
+      <About />
     </>
-  )
+  );
 }
 
 export default Index
